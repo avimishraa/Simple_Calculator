ff9451f1d1dcbc185460aaaec11b2b4864cd6d62
Avinash mishra <avimishra3752@gmail.com>
2025-07-15 14:10:18 +0530

    initial commit

---
+++ simpleCalculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    float num1, num2;
+
+    cout << "Enter the operator: "; cin >> op;
+    cout << "Enter the number:" << endl;   
+    cout << "a = "; cin >> num1;
+    cout << "b = "; cin >> num2;
+    cout << "Result [a " << op << " b] = ";
+
+    switch (op) {
+        case '+': cout << num1 + num2; break;
+        case '-': cout << num1 - num2; break;
+        case '*': cout << num1 * num2; break;
+        case '/': cout << num1 / num2; break;
+        default: cout << "Error! operator is not correct";
+    }
+
+    return 0;
+}
